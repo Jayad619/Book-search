0c19a5fdc26411f545246965e2dab5dfe80c8d8c
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-07 20:56:53 +0100

    resolver for add user mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,6 +13,15 @@ const resolvers = {
     },
   },
 
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+  },
+
 
 };
 
